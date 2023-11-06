0a1a930bd14aee6594b9405b72bcf7f5ba9b4d71
Ahmed bouakroucha <ahmed.bouakroucha@esprit.tn>
2023-11-06 15:41:21 -0800

    Controller product Test commit

--- DevOps_Project-20231016T100739Z-001/DevOps_Project/src/test/java/tn/esprit/devops_project/controllers/ProductControllerTest.java
+++ DevOps_Project-20231016T100739Z-001/DevOps_Project/src/test/java/tn/esprit/devops_project/controllers/ProductControllerTest.java
@@ -1,46 +1,107 @@
-package tn.esprit.devops_project.controllers;
-
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tn.esprit.devops_project.controllers.ProductController;
 import tn.esprit.devops_project.entities.Product;
 import tn.esprit.devops_project.entities.ProductCategory;
 import tn.esprit.devops_project.services.Iservices.IProductService;
 
+import java.util.Arrays;
 import java.util.List;
 
-@RestController
-@AllArgsConstructor
-@CrossOrigin(origins = "*")
-public class ProductController {
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-    private final IProductService productService;
+public class ProductControllerTest {
 
-    @PostMapping("/product/{idStock}")
-    Product addProduct(@RequestBody Product product,@PathVariable Long idStock){
-        return productService.addProduct(product,idStock);
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private IProductService productService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
     }
 
-    @GetMapping("/product/{id}")
-    Product retrieveProduct(@PathVariable Long id){
-        return productService.retrieveProduct(id);
+    @Test
+    public void testAddProduct() throws Exception {
+        Product product = new Product();
+        product.setId(1L);
+
+        when(productService.addProduct(any(Product.class), anyLong())).thenReturn(product);
+
+        mockMvc.perform(post("/product/{idStock}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":1, \"name\":\"Product1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
     }
 
-    @GetMapping("/product")
-    List<Product> retreiveAllProduct(){
-        return productService.retreiveAllProduct();
-    }
-    @GetMapping("/product/stock/{id}")
-    List<Product> retreiveProductStock(@PathVariable Long id){
-        return productService.retreiveProductStock(id);
+    @Test
+    public void testRetrieveProduct() throws Exception {
+        Product product = new Product();
+        product.setId(1L);
+
+        when(productService.retrieveProduct(1L)).thenReturn(product);
+
+        mockMvc.perform(get("/product/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
     }
 
-    @GetMapping("/productCategoy/{category}")
-    List<Product> retrieveProductByCategory(@PathVariable ProductCategory category){
-        return productService.retrieveProductByCategory(category);
+    @Test
+    public void testRetreiveAllProduct() throws Exception {
+        List<Product> products = Arrays.asList(new Product(), new Product());
+
+        when(productService.retreiveAllProduct()).thenReturn(products);
+
+        mockMvc.perform(get("/product"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2));
     }
 
-    @DeleteMapping("/product/{id}")
-    void deleteProduct(@PathVariable Long id){
-        productService.deleteProduct(id);
+    @Test
+    public void testRetreiveProductStock() throws Exception {
+        List<Product> products = Arrays.asList(new Product(), new Product());
+
+        when(productService.retreiveProductStock(1L)).thenReturn(products);
+
+        mockMvc.perform(get("/product/stock/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void testRetrieveProductByCategory() throws Exception {
+        ProductCategory category = ProductCategory.CATEGORY1;
+        List<Product> products = Arrays.asList(new Product(), new Product());
+
+        when(productService.retrieveProductByCategory(category)).thenReturn(products);
+
+        mockMvc.perform(get("/productCategoy/{category}", category))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void testDeleteProduct() throws Exception {
+        mockMvc.perform(delete("/product/{id}", 1))
+                .andExpect(status().isOk());
     }
 }
+
