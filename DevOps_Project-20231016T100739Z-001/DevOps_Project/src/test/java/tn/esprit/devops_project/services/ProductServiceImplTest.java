95ae509c0cbe72b871722dca5e3ad256af1ed5c5
Ahmed bouakroucha <ahmed.bouakroucha@esprit.tn>
2023-11-06 15:24:58 -0800

    service Test commit

--- DevOps_Project-20231016T100739Z-001/DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ DevOps_Project-20231016T100739Z-001/DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -1,54 +1,123 @@
-package tn.esprit.devops_project.services;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import tn.esprit.devops_project.services.Iservices.IProductService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.devops_project.entities.Product;
 import tn.esprit.devops_project.entities.ProductCategory;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.ProductRepository;
 import tn.esprit.devops_project.repositories.StockRepository;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-@Service
-@AllArgsConstructor
-@Slf4j
-public class ProductServiceImpl implements IProductService {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
-   final ProductRepository productRepository;
-   final StockRepository stockRepository;
+public class ProductServiceImplTest {
 
-    @Override
-    public Product addProduct(Product product, Long idStock) {
-        Stock stock = stockRepository.findById(idStock).orElseThrow(() -> new NullPointerException("stock not found"));
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private StockRepository stockRepository;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAddProduct() {
+        Stock stock = new Stock();
+        stock.setIdStock(1L);
+
+        Product product = new Product();
+        product.setId(1L);
         product.setStock(stock);
-        return productRepository.save(product);
+
+        Mockito.when(stockRepository.findById(1L)).thenReturn(Optional.of(stock));
+        Mockito.when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        Product savedProduct = productService.addProduct(product, 1L);
+
+        assertEquals(1L, savedProduct.getId().longValue());
+        assertEquals(stock, savedProduct.getStock());
     }
 
-    @Override
-    public Product retrieveProduct(Long id) {
-        return productRepository.findById(id).orElseThrow(() -> new NullPointerException("Product not found"));
+    @Test
+    public void testRetrieveProduct() {
+        Product product = new Product();
+        product.setId(1L);
+
+        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+
+        Product retrievedProduct = productService.retrieveProduct(1L);
+
+        assertEquals(1L, retrievedProduct.getId().longValue());
     }
 
-    @Override
-    public List<Product> retreiveAllProduct() {
-        return productRepository.findAll();
+    @Test
+    public void testRetreiveAllProduct() {
+        Product product1 = new Product();
+        product1.setId(1L);
+
+        Product product2 = new Product();
+        product2.setId(2L);
+
+        Mockito.when(productRepository.findAll()).thenReturn(Arrays.asList(product1, product2));
+
+        List<Product> products = productService.retreiveAllProduct();
+
+        assertEquals(2, products.size());
     }
 
-    @Override
-    public List<Product> retrieveProductByCategory(ProductCategory category) {
-        return productRepository.findByCategory(category);
+    @Test
+    public void testRetrieveProductByCategory() {
+        ProductCategory category = ProductCategory.CATEGORY1;
+
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setCategory(category);
+
+        Product product2 = new Product();
+        product2.setId(2L);
+        product2.setCategory(category);
+
+        Mockito.when(productRepository.findByCategory(category)).thenReturn(Arrays.asList(product1, product2));
+
+        List<Product> products = productService.retrieveProductByCategory(category);
+
+        assertEquals(2, products.size());
     }
 
-    @Override
-    public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
+    @Test
+    public void testDeleteProduct() {
+        productService.deleteProduct(1L);
+
+        Mockito.verify(productRepository).deleteById(1L);
     }
 
-    @Override
-    public List<Product> retreiveProductStock(Long id) {
-        return productRepository.findByStockIdStock(id);
+    @Test
+    public void testRetreiveProductStock() {
+        Long stockId = 1L;
+
+        Product product1 = new Product();
+        product1.setId(1L);
+
+        Product product2 = new Product();
+        product2.setId(2L);
+
+        Mockito.when(productRepository.findByStockIdStock(stockId)).thenReturn(Arrays.asList(product1, product2));
+
+        List<Product> products = productService.retreiveProductStock(stockId);
+
+        assertEquals(2, products.size());
     }
 }
